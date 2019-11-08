699539eee03b61f2140284e2f1c2ed2274b11d3b
weiyuan95 <lee.wei.yuan-95@hotmail.com>
2019-11-08 23:54:21 +0800

    integrate image and video carousel into 1 component

---
+++ src/components/dashboard/MediaContainer.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-tabs
+    background-color="primary"
+    :icons-and-text="icons"
+  >
+    <v-tab v-for="mediaComponent in mediaComponents" :key="mediaComponent.name">
+      {{ mediaComponent.name }}
+    </v-tab>
+
+    <v-tab-item v-for="mediaComponent in mediaComponents" :key="mediaComponent.name">
+      <v-card flat tile>
+        <keep-alive>
+          <component :is="mediaComponent.component"></component>
+        </keep-alive>
+      </v-card>
+    </v-tab-item>
+  </v-tabs>
+</template>
+
+<script>
+import ImageCarousel from "./ImageCarousel.vue";
+import VideoFeature from "./VideoFeature.vue";
+
+export default {
+  data() {
+    return {
+      mediaComponents: [
+        {
+          name: "Video",
+          component: VideoFeature
+        },
+        {
+          name: "Gallery",
+          component: ImageCarousel
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
